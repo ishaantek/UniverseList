89bfc67fd9059fbffdbcca0003e2e66216d1abad
Ishaan Garg <ishaan@ishaantek.com>
2022-11-24 19:16:39 -0500

    Added user DM

--- mainbot/events/guildMemberAdd.js
+++ mainbot/events/guildMemberAdd.js
@@ -1,3 +1,10 @@
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+
 module.exports = {
   async run(client, member) {
     const id = member.guild.id;
@@ -14,7 +21,63 @@ module.exports = {
           `<:awesome:1043642149100601435> \`${member.user.username}\` has joined the server!`
         )
         .catch(() => null);
-      // member.roles.add("1006067703238369291").catch(() => null);
+      member.roles.add("1045107993563373578").catch(() => null);
+      const embed = new EmbedBuilder()
+        .setAuthor({
+          name: member.user.tag,
+          iconURL: member.user.displayAvatarURL({ dyncamic: true }),
+        })
+        .setThumbnail(member.user.displayAvatarURL({ dyncamic: true }))
+        .setTitle("Member Joined")
+        .setDescription(`${member.user} has joined the server.`)
+        .addFields({
+          name: "Joined Discord",
+          value: `<t:${Math.floor(member.user.createdTimestamp / 1000)}:R>`,
+          inline: true,
+        })
+        .addFields({
+          name: "Joined Server",
+          value: `<t:${Math.floor(member.joinedTimestamp / 1000)}:R>`,
+          inline: true,
+        })
+        .addFields({ name: "Bot", value: `${member.user.bot}`, inline: true })
+        .setFooter({
+          text: "Universe List - Member Join Logs",
+          iconURL: client.user.displayAvatarURL(),
+        });
+      client.channels.resolve("947636978130751578").send({ embeds: [embed] });
+
+      try {
+        const embed = new EmbedBuilder()
+          .setTitle("Welcome to Universe List!")
+          .setDescription(
+            "Welcome to Universe List, one of the most powerful bot and server listing site!.\nIf you have any questions or concerns, please contact support.\nHave a wonderful day!"
+          )
+          .setFooter({
+            text: `Please contact support to stop these messages. - Universe List`,
+            iconURL: `https://universe-list.xyz/img/icon.png`,
+          });
+
+        let row = new ActionRowBuilder();
+
+        row.addComponents(
+          new ButtonBuilder()
+            .setURL("https://universe-list.xyz")
+            .setLabel("Universe List")
+            .setStyle(ButtonStyle.Link)
+        );
+
+        row.addComponents(
+          new ButtonBuilder()
+            .setURL("https://universe-list.xyz/bots/new")
+            .setLabel("Add A Bot")
+            .setStyle(ButtonStyle.Link)
+        );
+
+        member.send({ embeds: [embed], components: [row] });
+      } catch (err) {
+        console.error("Message failed to send to user: " + err);
+      }
     }
   },
 };
