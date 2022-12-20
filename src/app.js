942bf148269bd6fd28c4263400732df8f402916b
Ishaan Garg <ishaan@ishaantek.com>
2022-12-19 22:24:31 -0500

    Fixed

--- src/app.js
+++ src/app.js
@@ -1036,10 +1036,7 @@ app.get("/servers/:id/edit", checkAuth, async (req, res) => {
   const server = await global.serverModel.findOne({ id: id });
   if (!server) return res.redirect("/404");
 
-  if (
-    req.user.id !== server.owner ||
-    req.user.roles.cache.some((role) => role.id === config.roles.bottester)
-  )
+  if (req.user.id !== server.owner)
     return res.redirect("/403");
 
   const ServerRaw = (await global.sclient.guilds.fetch(id)) || null;
