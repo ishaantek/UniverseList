ac7963f2046efe938cb6ee71739abd3cdb8ed082
bluefish1678 <105519606+bluefish1678@users.noreply.github.com>
2023-01-18 17:23:22 -0800

    Update guildCreate.js

--- src/servers/events/guildCreate.js
+++ src/servers/events/guildCreate.js
@@ -33,6 +33,23 @@ module.exports = {
         text: `Universe Servers - Guild Logs`,
         iconURL: `${global.sclient.user.displayAvatarURL()}`,
       });
+    
+    
+      const welcomeEmbed = new EmbedBuilder()
+       .setTitle("Thanks for inviting Universe Servers!")
+       .setThumbnail(`${guild.iconURL({ dynamic: true })}`)
+       .setDescription(
+         `Visit my website at https://universe-list.xyz\n**Got Questions?** Join our support server below and ask our freindly staff team!\n\nhttps://universe-list.xyz `
+         `Visit my website at https://universe-list.xyz\n**Got Questions?** Join our support server below and ask our friendly staff team!\n\nhttps://universe-list.xyz `
+       )
+ 
+      .setFooter({
+        text: `Universe Servers - Guild Logs`,
+        iconURL: `${global.sclient.user.displayAvatarURL()}`,
+      });
+        const channels = guild.channels.cache.filter(channel => channel.type == "text");
+        channels.first().send({ embeds: [welcomeEmbed] }).catch(e => console.log(e));
+        
     return logs.send({ embeds: [embed] });
   },
 };
