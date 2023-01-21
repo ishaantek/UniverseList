07dfe6a786a5317fee59e41865d1c3266e1077aa
Ishaan Garg <ishaan@ishaantek.com>
2023-01-21 02:11:52 -0500

    Added uptime command

--- mainbot/commands/ping.js
+++ mainbot/commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "ping",
   aliases: ["latency"],
-  description: "Check the bots ping. (Delay)",
+  description: "Check the bot's ping.",
   async run(client, message, args) {
     return await message.reply({
       content: `<:pong:1016422519214964766> Ping: \`${client.ws.ping}ms\``,
