ad7a994401e9552c6b94fa856fd0f1f3f752177a
Ishaan Garg <ishaan@ishaantek.com>
2023-02-02 17:50:49 -0500

    Fixed auto responder image

--- mainbot/commands/ar-wrongserver.js
+++ mainbot/commands/ar-wrongserver.js
@@ -5,8 +5,10 @@ module.exports = {
   async run(client, message, args) {
     const embed = new EmbedBuilder()
       .setColor("7289da")
-      .setImage("https://i.ibb.co/9nzmCsD/thign.png")
-      .setDescription(`<:ul_dotBlue:1017569447852130324> Hey! We think you have our server mistaken. We do not provide support, help, or advice for any bot. You need to click on the "Support" button on the bot's page of the bot you need support for. If there isn't a button that says " Support" or nothing else mentioned about a support server, then the best option is to try and DM the owner.`);
+      .setImage("https://i.ibb.co/51dTL4J/image.png")
+      .setDescription(
+        `<:ul_dotBlue:1017569447852130324> Hey! We think you have our server mistaken. We do not provide support, help, or advice for any bot. You need to click on the "Support" button on the bot's page of the bot you need support for. If there isn't a button that says " Support" or nothing else mentioned about a support server, then the best option is to try and DM the owner.`
+      );
     return message.channel.send({ embeds: [embed] });
   },
 };
