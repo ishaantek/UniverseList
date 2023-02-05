4e2623e4f816cdf3a2d1902c5ee13627e21ed191
Ishaan Garg <ishaan@ishaantek.com>
2023-02-04 21:17:09 -0500

    Created invite page

--- src/app.js
+++ src/app.js
@@ -1860,6 +1860,10 @@ app.get("/bot-requirements", async (req, res) => {
   res.render("legal/bot-requirements.ejs", { user: req.user });
 });
 
+app.get("/invite", async (req, res) => {
+  res.render("invite.ejs", { user: req.user });
+});
+
 app.get("/403", async (req, res) => {
   res.render("errors/403.ejs", { user: req.user });
 });
