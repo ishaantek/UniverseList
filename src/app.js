85b47cfba7196277601c19435d24e44aeef5949f
Apteryx <apteryx.xyz@gmail.com>
2023-04-17 10:19:39 +1200

    Fix login return to not working (#34)
    * Fix login return to not working
    
    * Fix logout return to not working

--- src/app.js
+++ src/app.js
@@ -153,26 +153,24 @@ let normalScopes = ["identify"];
 
 app.get(
   "/auth/login",
-  passport.authenticate("discord", {
-    scope: normalScopes,
-    prompt: prompt,
-    callbackURL: config.bot.redirect,
-  }),
-  (req, res) => {
-    if (req.query.from) req.session.returnTo = req.query.from;
-  }
+  (req, res, next) =>
+    passport.authenticate("discord", {
+      scope: normalScopes,
+      prompt: prompt,
+      callbackURL: config.bot.redirect,
+      state: req.query.from || '/',
+    })(req, res, next)
 );
 
 app.get(
   "/auth/login/joinSupport",
-  passport.authenticate("discord", {
-    scope: scopes,
-    prompt: prompt,
-    callbackURL: `${config.bot.redirect}/joinSupport`,
-  }),
-  (req, res) => {
-    if (req.query.from) req.session.returnTo = req.query.from;
-  }
+  (req, res, next) => 
+    passport.authenticate("discord", {
+      scope: scopes,
+      prompt: prompt,
+      callbackURL: `${config.bot.redirect}/joinSupport`,
+      state: req.query.from || '/',
+    })(req, res, next)
 );
 
 app.get(
@@ -181,7 +179,7 @@ app.get(
     failureRedirect: "/",
   }),
   function (req, res) {
-    res.redirect(req.session.returnTo || "/");
+    res.redirect(req.query.state || "/");
   }
 );
 
@@ -212,7 +210,7 @@ app.get(
       );
     } catch {}
 
-    res.redirect(req.session.returnTo || "/");
+    res.redirect(req.query.state || "/");
   }
 );
 
@@ -222,11 +220,8 @@ app.get("/info", async (req, res) => {
 
 app.get("/auth/logout", function (req, res) {
   req.logout(() => {
-    res.redirect("/");
+    res.redirect(req.query.from || "/");
   });
-  if (req.session.returnTo) {
-    delete req.session.returnTo;
-  }
 });
 
 //-bot-//
