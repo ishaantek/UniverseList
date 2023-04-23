ccb9e799ba5460ca655ec1b2b6190b0951f1fd8d
Tejas Lamba <73745640+TejasLamba2006@users.noreply.github.com>
2023-04-22 21:23:42 -0700

    Update level.js

--- mainbot/commands/level.js
+++ mainbot/commands/level.js
@@ -1,41 +1,41 @@
 const canvacord = require("canvacord");
 const { AttachmentBuilder } = require("discord.js");
+let model = require("../../src/models/user.js");
 module.exports = {
   name: "level",
   aliases: ["rank", "lvl"],
   description: "Rank command for users to check their level.",
   async run(_, message) {
-    let model = require("../../src/models/user.js");
-    let user = await model.findOne({ id: message.author.id });
-    if (!user) {
-      user = new model({
-        id: message.author.id,
-        xp: 0,
-        level: 0,
-      });
-      user.save();
-    }
-    const img = message.author.displayAvatarURL();
-    let level = user.level + 1;
-    let flitered = await model.find({}).sort({ xp: -1 }).limit(10);
-    let sorted = flitered.map((x) => x.xp).sort((a, b) => b - a);
-    let rank = sorted.splice(0, message.guild.memberCount);
-    let background = "https://cdn.discordapp.com/attachments/941896555718410285/1054106088171110480/image.png";
-    let rankIndex = rank.indexOf(user.xp) + 1;
-    const userrank = new canvacord.Rank()
-      .setAvatar(img)
-      .setCurrentXP(user.xp)
-      .setRequiredXP(level * 50)
-      .setStatus("online")
-      .setBackground("IMAGE", background)
-      .setLevel(user.level)
-      .setRank(rankIndex)
-      .setProgressBar("#FFFFFF", "COLOR")
-      .setUsername(message.author.username)
-      .setDiscriminator(message.author.discriminator);
-    userrank.build().then((data) => {
-      const attachment = new AttachmentBuilder(data, { name: "RankCard.png" });
-      message.reply({ files: [attachment] });
-    });
+let user = await model.findOne({ id: message.author.id });
+if (!user) {
+  user = new model({
+    id: message.author.id,
+    xp: 0,
+    level: 0,
+  });
+  user.save();
+}
+const img = message.author.displayAvatarURL();
+let level = user.level + 1;
+let flitered = await model.find({}).sort({ xp: -1 });
+let rank = flitered.findIndex((x) => x.id === user.id) + 1;
+let background = "https://cdn.discordapp.com/attachments/941896555718410285/1054106088171110480/image.png";
+//let rankIndex = rank.includes(user.xp) ? rank.indexOf(user.xp) + 1 : "Not ranked";
+const userrank = new canvacord.Rank()
+  .setAvatar(img)
+  .setCurrentXP(user.xp)
+  .setRequiredXP(level * 50)
+  .setStatus("online")
+  .setBackground("IMAGE", background)
+  .setLevel(user.level)
+  .setRank(rank)
+  .setProgressBar("#FFFFFF", "COLOR")
+  .setUsername(message.author.username)
+  .setDiscriminator(message.author.discriminator);
+userrank.build().then((data) => {
+  const attachment = new AttachmentBuilder(data, { name: "RankCard.png" });
+  message.reply({ files: [attachment] });
+});
+
   },
 };
