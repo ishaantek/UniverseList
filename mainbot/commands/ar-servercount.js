cab2202437d360ec0f9bc5b5076a06f52f464698
Ishaan Garg <ishaan@ishaantek.com>
2023-04-25 18:26:08 -0400

    Added servercount auto responder

---
+++ mainbot/commands/ar-servercount.js
@@ -0,0 +1,13 @@
+const { EmbedBuilder } = require("discord.js");
+module.exports = {
+  name: "servercount",
+  aliases: ["sc"],
+  async run(client, message, args) {
+    const embed = new EmbedBuilder()
+      .setColor("7289da")
+      .setDescription(
+        `To display your bot's server count on the Universe List, please refer to the [API documentation](https://universe-list.xyz/docs) regarding server/shard posting.\n\nAlternatively, if your bot is developed using Discord.js, you can utilize our [NPM Package](https://www.npmjs.com/package/universe-list.js).`
+      );
+    return message.channel.send({ embeds: [embed] });
+  },
+};
