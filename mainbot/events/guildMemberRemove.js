4cbb94df42e2a1d479bd009afc1131f1f5344893
Ishaan Garg <ishaan@ishaantek.com>
2023-05-02 00:10:24 -0400

    Minor typo

--- mainbot/events/guildMemberRemove.js
+++ mainbot/events/guildMemberRemove.js
@@ -10,9 +10,9 @@ module.exports = {
         const bot_kick = new EmbedBuilder()
           .setAuthor({
             name: member.user.tag,
-            iconURL: member.user.displayAvatarURL({ dyncamic: true }),
+            iconURL: member.user.displayAvatarURL({ dynamic: true }),
           })
-          .setThumbnail(member.user.displayAvatarURL({ dyncamic: true }))
+          .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
           .setTitle("Bot Kicked")
           .setDescription(
             `${member.user} (${member.user.tag}) has left the server as a result of their **${bots.length} bots** being removed from the list and kicked from the server.`
@@ -44,9 +44,9 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setAuthor({
         name: member.user.tag,
-        iconURL: member.user.displayAvatarURL({ dyncamic: true }),
+        iconURL: member.user.displayAvatarURL({ dynamic: true }),
       })
-      .setThumbnail(member.user.displayAvatarURL({ dyncamic: true }))
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
       .setTitle("Member Left")
       .setDescription(`${member.user} has left the server.`)
       .addFields({
