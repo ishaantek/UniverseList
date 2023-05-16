81096af2fbd9f187fa203887ed054d147d58e390
Tejas Lamba <tejas22feb@gmail.com>
2023-05-16 12:59:31 +0530

    Add visa2discord dependency and revise eval command
    The commit adds a new dependency named "visa2discord" and updates the `eval` command to make use of the new dependency, improving code security. Instead of using `eval-helper`, it now uses node's native `eval` function to get evaluated output, avoiding specific string manipulations that could make the bot vulnerable. Additionally, the commit renames `evaled` to `code` to improve readability, and creates a new `splitDescription` variable to split the output string into multiple messages if needed.

--- mainbot/commands/eval.js
+++ mainbot/commands/eval.js
@@ -1,9 +1,8 @@
+const { cleanCode, splitMessageRegex } = require("visa2discord")
+const { EmbedBuilder } = require("discord.js")
 const {
-    getCode,
-    clean
-} = require("@elara-services/eval-helper");
-
-
+  inspect
+} = require(`util`);
 module.exports = {
 
     name: "eval",
@@ -14,29 +13,37 @@ module.exports = {
             return message.reply({
               content: `<:no:946581450600370298> You must provide a code to evaluate.`,
             });
-        const evaled = await getCode({
-            code: args.join(" "),
-            async: false,
+        const code = args.join(" ");
+        if (args.includes(`token`))
+        return message.reply({ content: `No token grabbing for you!` });
+      //get the evaled content
+      evaled = await eval(args);
+      //make string out of the evaluation
+      let string = inspect(evaled);
+      //if the token is included return error
+      if (string.includes(client.token))
+        return message.reply({ content: `No token grabbing for you!` });
+      //define queueembed
+      let evalEmbed = new EmbedBuilder()
+        .setTitle(`${client.user.username} | Evaluation`)
+        .setColor("ffc0cb");
+           
+      //split the description
+      const splitDescription = splitMessageRegex(string, {
+        maxLength: 2000,
+        char: `\n`,
+        prepend: ``,
+        append: ``,
+      });
+  
+      //For every description send a new embed
+      splitDescription.forEach(async (m) => {
+   
+        //(over)write embed description
+        evalEmbed.setDescription(`\`\`\`` + m + `\`\`\``);
+        //send embed
+        
+        message.channel.send({embeds: [evalEmbed]});
         });
-        const code = await clean(eval(evaled), [
-            client.token
-        ]);
-        try {
-            if (typeof code !== "string") {
-              return message.channel.send({
-                content: `<:yes:946581450852016198> Successfully evaluated.\n\`\`\`js\n${code}\n\`\`\``,
-              });
-            } else {
-              return message.channel.send({
-                content: `<:yes:946581450852016198> Successfully evaluated.\n\`\`\`js\n${code}\n\`\`\``,
-              });
-            }
-        } catch (e) {
-            message.channel.send({
-              content:
-                "<:no:946581450600370298> There was an error during evaluation.\n" +
-                e,
-            });
-        }
     },
 };
